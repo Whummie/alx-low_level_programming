a9f38de5007c278fcc70ba92bfcda0621c0f31d1
Whummie <olawumi.oyelade@gmail.com>
2022-09-14 15:00:44 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,14 +22,14 @@ void print_times_table(int n)
 				if (result <= 99)
 				_putchar (' ');
 
-				trimif (result <= 9)			
+				if (result <= 9)			
 				_putchar(' ');
-				trimif (result >= 100)	
+				if (result >= 100)	
 				{
 										_putchar((result / 100) + '0');
 	_putchar((result / 10) % 10 + '0');
 				}								else if (result <= 99 && result >= 10)
-				trim{							
+				{							
 					_putchar((result / 10) + '0');
 										}
 					_putchar((result % 10) + '0');
