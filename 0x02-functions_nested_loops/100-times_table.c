5b3b094d116613af013a45db875396c7d3ee0d79
Whummie <olawumi.oyelade@gmail.com>
2022-09-14 15:35:17 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,15 +21,14 @@ void print_times_table(int n)
 				result = digit * mult;
 				if (result <= 99)
 				_putchar (' ');
-
 				if (result <= 9)			
 				_putchar(' ');
 				if (result >= 100)	
 				{
-										_putchar((result / 100) + '0');
+					_putchar((result / 100) + '0');
 	_putchar((result / 10) % 10 + '0');
 				}								else if (result <= 99 && result >= 10)
-				{							
+				{
 					_putchar((result / 10) + '0');
 										}
 					_putchar((result % 10) + '0');
