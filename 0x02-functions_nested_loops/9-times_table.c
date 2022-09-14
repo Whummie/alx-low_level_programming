bd4508f657d9915a1d3e265031de51c62e3a192f
Whummie <olawumi.oyelade@gmail.com>
2022-09-14 12:49:59 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 1; mult <= 9; mult++)
+		for (mult = 1; mulit <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -21,6 +21,7 @@ void times_table(void)
 				_putchar (' ');
 			else 
 				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar ('\n');
 	}
