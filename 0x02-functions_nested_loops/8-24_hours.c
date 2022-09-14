20a788911a3264a33757309ed202504e768eb4ac
Whummie <olawumi.oyelade@gmail.com>
2022-09-14 12:13:26 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - func that prints every minute of the day of Jack Bauer, starting
+ * jack_bauer - func that prints every minute of the day of Jack Bauer
  * n from 00:00 to 23:59, min loop counts mins, while hour loop count hours
  * and resets mins
  * Return: 0
