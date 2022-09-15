89e77efc568992cc5bbf5bd65e786402b796f32d
Whummie <olawumi.oyelade@gmail.com>
2022-09-15 13:56:10 -0700

    add

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,16 +3,16 @@
 
 /**
  * print_square - prints a square
- * @size: the size of the square
+ *  @size: the size of the square
  */
 
 void print_square(int size)
 {
 	int i, j;
 
-	if (size > 0)			
+
+	if (size > 0)
 	{
-	
 		for (i = 0; i < size; ++i)
 		{
 			for (j = 0; j < size; j++)
@@ -25,4 +25,3 @@ void print_square(int size)
 	else
 		_putchar('\n');
 }
-
