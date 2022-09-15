5dd0835db58a793409d6d8265667925ee067d410
Whummie <olawumi.oyelade@gmail.com>
2022-09-15 12:49:12 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,9 +6,9 @@
 
 void print_numbers(void)
 {
-		int i;
+	int i;
 
-			for (i = '0'; i <= '9'; i++)
-						_putchar(i);
-				_putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
