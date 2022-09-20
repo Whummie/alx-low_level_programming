2244d201d57002e6cb154a88b65a224fb537075e
Whummie <olawumi.oyelade@gmail.com>
2022-09-20 12:21:59 -0700

    print

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
 /**
  * print_array - prints n elements of an array of integers
  * @a: A pointer to an int that will be updated/changed
- * @n: return value 
+ * @n: return value n
  * Return: void which means our answer is correct
  */
 void print_array(int *a, int n)
@@ -15,9 +15,8 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 		printf("%d", a[i]);
-	
-		if (i < n - 1)
-			
+
+		if (i < n - 1)			
 		{
 			printf(", ");
 		}
